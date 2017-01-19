ddde40b4dd717f09a9a252c8e42964d578083dfe
Chad Swatsworth <Chad Swatsworth>
2017-01-19 09:52:18 -0500

    passed in two

--- test_functions.rb
+++ test_functions.rb
@@ -1,8 +1,8 @@
 require 'minitest/autorun'
 require_relative 'functions.rb'
 	def mined_minds_function(number)
-	2
-end
+		number
+	end
 
 class TestMMFunctions < Minitest::Test
 	def test_one_equals_one()
@@ -10,7 +10,9 @@ class TestMMFunctions < Minitest::Test
 	end
 
 	def test_pass_1_to_mined_minds_function_returns_1
+		assert_equal(1, mined_minds_function(1))
+	end
+	def test_pass_2_to_mined_minds_function_returns_2
 		assert_equal(2, mined_minds_function(2))
 	end
-
 end
