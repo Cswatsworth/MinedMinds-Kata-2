c7d3efa4e9bbc2c290ac729529beb5439ee006c9
Chad Swatsworth <Chad Swatsworth>
2017-01-19 10:25:06 -0500

    9 to mined

--- test_functions.rb
+++ test_functions.rb
@@ -10,10 +10,16 @@ class TestMMFunctions < Minitest::Test
 	def test_pass_1_to_mined_minds_function_returns_1
 		assert_equal(1, mined_minds_function(1))
 	end
+	
 	def test_pass_2_to_mined_minds_function_returns_2
 		assert_equal(2, mined_minds_function(2))
 	end
-	def test_pass_2_to_mined_minds_function_returns_mined
-		assert_equal('mined', mined_minds_function(3))
+	
+	def test_pass_3_to_mined_minds_function_returns_mined
+		assert_equal('mined', mined_minds_function(3))	
+	end
+
+	def test_pass_4_to_mined_minds_function_returns_mined
+		assert_equal('mined', mined_minds_function(9))
 	end
 end
